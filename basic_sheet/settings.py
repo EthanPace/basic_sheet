9105ce06af0fde52f32c94ae414d821c11f50290
Ethan Pace <ethanpace212@gmail.com>
2023-05-26 20:12:40 +1000

    added app and set allowed hosts all

--- basic_sheet/settings.py
+++ basic_sheet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0s^#y+5saf!@@dgls$9))20^(ifnx*2g89oo!j*5q$2u9#0tuo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sheet',
 ]
 
 MIDDLEWARE = [
